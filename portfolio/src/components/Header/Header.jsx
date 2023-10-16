c24a09593c26b03cd8e451b077c6fac9bf99bbed
Yevhenii Lymarenko <96422034+YevheniiLi@users.noreply.github.com>
2023-10-16 16:55:47 +0200

    create section header, add frame motion

---
+++ portfolio/src/components/Header/Header.jsx
@@ -0,0 +1,33 @@
+import css from './Header.module.scss'
+import {BiPhoneCall} from 'react-icons/bi'
+import {motion} from 'framer-motion';
+import { headerVariants } from '../../utils/motion';
+
+const Header = () => {
+  return (
+<motion.div 
+initial="hidden"
+whileInView="show"
+variants={headerVariants}
+viewport={{once: true, amount: 0.25}}
+className={`paddings ${css.wrapper}`}>
+  <div className={`flexCenter innerWidth ${css.container}`}>
+    <div className={css.name}>
+      Yevhenii
+    </div>
+    <ul className={`flexCenter ${css.menu}`}>
+      <li><a href="">Services</a></li>
+      <li><a href="">Experience</a></li>
+      <li><a href="">Portfolio</a></li>
+      <li><a href="">Testimonials</a></li>
+      <li className={`flexCenter ${css.phone}`}>
+            <p>+001 (313) 345 678</p>
+            <BiPhoneCall size={"40px"} />
+          </li>
+    </ul>
+  </div>
+</motion.div>
+  )
+}
+
+export default Header
